54e0ac9f223fc81d0165053850dcfa251195e08d
Meltem Ugurlu <ugurlumeltem7@icloud.com>
2023-06-01 16:10:01 +0300

    Initial Commit

---
+++ FavoriteBook/FavoriteBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteBookApp.swift
+//  FavoriteBook
+//
+//  Created by Meltem Uğurlu on 1.06.2023.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
